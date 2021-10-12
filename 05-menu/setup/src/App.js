e3577f5f52d0a102e6b4745da0695cb8a70d984f
systemRockstar <18580997+systemRockstar@users.noreply.github.com>
2021-10-12 14:05:42 +0530

    added functionality

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,16 +3,27 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))]
+
 function App() {
   const [menuItems, setMenuItems] = useState(items)
-  const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState(allCategories)
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items)
+    } else {
+      const newItems = items.filter((item) => item.category === category)
+      setMenuItems(newItems)
+    }
+  }
 
   return <main>
     <section className="menu section">
       <div>
         <h2 className="title">our menu</h2>
         <div className="underline"></div>
-        <Categories />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </div>
     </section>
--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ categories, filterItems }) => {
+  return <div className="btn-container">
+    {categories.map((category) => {
+      return <button className="filter-btn" onClick={() => filterItems(category)}>{category}</button>
+    })}
+  </div>;
 };
 
 export default Categories;
