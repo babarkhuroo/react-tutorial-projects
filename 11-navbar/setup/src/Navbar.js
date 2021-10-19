9e641b0ad03e01f64b689a058f3d260dd0da1402
systemRockstar <18580997+systemRockstar@users.noreply.github.com>
2021-10-19 12:39:13 +0530

    added nav-toggle

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -4,16 +4,31 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false)
+  const linksContainerRef = useRef(null)
+  const linksRef = useRef(null)
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height
+    console.log(linksContainerRef.current.getBoundingClientRect())
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`
+    } else {
+      linksContainerRef.current.style.height = '0px'
+    }
+  }, [showLinks])
+
   return <nav>
     <div className="nav-center">
       <div className="nav-header">
         <img src={logo} alt="logo" />
-        <button className="nav-toggle">
+        <button className="nav-toggle" onClick={() => setShowLinks(!showLinks)}>
           <FaBars />
         </button>
       </div>
-      <div className="links-container show-container">
-        <ul className="links">
+
+      <div className='links-container' ref={linksContainerRef}>
+        <ul className="links" ref={linksRef}>
           {links.map((link) => {
             const { id, url, text } = link
             return <li key={id}>
@@ -22,9 +37,10 @@ const Navbar = () => {
           })}
         </ul>
       </div>
+
       <ul className="social-icons">
-        {social.map((soc) => {
-          const { id, url, icon } = soc
+        {social.map((socialIcon) => {
+          const { id, url, icon } = socialIcon
           return <li key={id}>
             <a href={url}>{icon}</a>
           </li>
