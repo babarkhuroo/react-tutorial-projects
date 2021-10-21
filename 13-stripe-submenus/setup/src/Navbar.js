67333cd91a2b02eb177078f88b133f32a6c26781
systemRockstar <18580997+systemRockstar@users.noreply.github.com>
2021-10-21 16:22:13 +0530

    added nav submenus

--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -5,7 +5,22 @@ import { useGlobalContext } from './context'
 
 const Navbar = () => {
   const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext()
-  return <nav className="nav">
+
+  const displaySubmenu = (e) => {
+    const page = e.target.textContent
+    const tempBtn = e.target.getBoundingClientRect()
+    const center = (tempBtn.left + tempBtn.right) / 2
+    const bottom = tempBtn.bottom - 3
+    openSubmenu(page, { center, bottom })
+  }
+
+  const handleSubmenu = (e) => {
+    if (!e.target.classList.contains('link-btn')) {
+      closeSubmenu()
+    }
+  }
+
+  return <nav className="nav" onMouseOver={handleSubmenu}>
     <div className="nav-center">
       <div className="nav-header">
         <img src={logo} alt="stripe" />
@@ -15,17 +30,17 @@ const Navbar = () => {
       </div>
       <ul className="nav-links">
         <li>
-          <button className="link-btn">
+          <button className="link-btn" onMouseOver={displaySubmenu}>
             products
           </button>
         </li>
         <li>
-          <button className="link-btn">
+          <button className="link-btn" onMouseOver={displaySubmenu}>
             developers
           </button>
         </li>
         <li>
-          <button className="link-btn">
+          <button className="link-btn" onMouseOver={displaySubmenu}>
             company
           </button>
         </li>
--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -5,7 +5,9 @@ const AppContext = React.createContext()
 
 export const AppProvider = ({ children }) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false)
-    const [isSubmenuOpen, setIsSubmenuOpen] = useState(true)
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false)
+    const [location, setLocation] = useState({})
+    const [page, setPage] = useState({ page: '', links: [] })
 
     const openSidebar = () => {
         setIsSidebarOpen(true)
@@ -13,14 +15,17 @@ export const AppProvider = ({ children }) => {
     const closeSidebar = () => {
         setIsSidebarOpen(false)
     }
-    const openSubmenu = () => {
+    const openSubmenu = (text, coordinates) => {
+        const page = sublinks.find((link) => link.page === text)
+        setPage(page)
+        setLocation(coordinates)
         setIsSubmenuOpen(true)
     }
     const closeSubmenu = () => {
         setIsSubmenuOpen(false)
     }
 
-    return <AppContext.Provider value={{ isSidebarOpen, isSubmenuOpen, openSidebar, openSubmenu, closeSidebar, closeSubmenu }}>
+    return <AppContext.Provider value={{ isSidebarOpen, isSubmenuOpen, openSidebar, openSubmenu, closeSidebar, closeSubmenu, location, page }}>
         {children}
     </AppContext.Provider>
 }
