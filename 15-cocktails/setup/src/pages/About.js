2e31e1a2564524752f79912afed34861b3ab63da
systemRockstar <18580997+systemRockstar@users.noreply.github.com>
2021-10-27 10:13:09 +0530

    added semi 15-cocktails

---
+++ 15-cocktails/setup/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, dolorem inventore, ut at reprehenderit quisquam ab animi amet repellendus voluptatem ipsum, autem ipsam fuga. Voluptas, quae sunt voluptatum deleniti dolore illum fuga voluptatibus aliquid deserunt, repudiandae amet placeat, in quasi.</p>
+    </section>
+  )
+}
+
+export default About
