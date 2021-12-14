5c49c1d63f6a75cfd7334111cab7e3bbd51e1442
systemRockstar <18580997+systemRockstar@users.noreply.github.com>
2021-12-14 16:57:21 +0530

    added 18

---
+++ 18-pagination/setup/src/Follower.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Follower = ({ login, avatar_url, html_url }) => {
+  return <article className="card">
+    <img src={avatar_url} alt={login} />
+    <h4>{login}</h4>
+    <a href={html_url} className='btn'>view profile</a>
+  </article>
+}
+
+export default Follower
