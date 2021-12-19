65d30a12453d7dfa4990aa871590e7928b08b2a3
systemRockstar <18580997+systemRockstar@users.noreply.github.com>
2021-12-19 22:47:48 +0530

    added 21

---
+++ 21-movie-db/setup/src/context.js
@@ -0,0 +1,22 @@
+import React, { useState, useContext, useEffect } from 'react'
+import useFetch from './useFetch'
+// make sure to use https
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) => {
+  const [query, setQuery] = useState('batman')
+  const { isLoading, error, data: movies } = useFetch(`&s=${query}`)
+
+  return (
+    <AppContext.Provider value={{ isLoading, error, movies, query, setQuery }}>
+      {children}
+    </AppContext.Provider>
+  )
+}
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export { AppContext, AppProvider }
