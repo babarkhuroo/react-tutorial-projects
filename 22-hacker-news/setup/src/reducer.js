49608934e3a49b7651008ac5bf8173e36864c58a
systemRockstar <18580997+systemRockstar@users.noreply.github.com>
2021-12-29 18:48:58 +0530

    initial commit for 22

---
+++ 22-hacker-news/setup/src/reducer.js
@@ -0,0 +1,17 @@
+import {
+  SET_LOADING,
+  SET_STORIES,
+  REMOVE_STORY,
+  HANDLE_PAGE,
+  HANDLE_SEARCH,
+} from './actions'
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_LOADING:
+      return { ...state, isLoading: true }
+    default:
+      throw new Error(`no matching "${action.type} action type`)
+  }
+}
+export default reducer
