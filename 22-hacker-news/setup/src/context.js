8e1c3d6f963189173235465b80eec7f1442a110d
systemRockstar <18580997+systemRockstar@users.noreply.github.com>
2021-12-29 20:56:34 +0530

    added 22

--- 22-hacker-news/setup/src/context.js
+++ 22-hacker-news/setup/src/context.js
@@ -42,11 +42,21 @@ const AppProvider = ({ children }) => {
     dispatch({ type: REMOVE_STORY, payload: id })
   }
 
+  const handleSearch = (query) => {
+    dispatch({ type: HANDLE_SEARCH, payload: query })
+  }
+
+  const handlePage = (value) => {
+    dispatch({ type: HANDLE_PAGE, payload: value })
+  }
+
   useEffect(() => {
     fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`)
-  }, [])
+  }, [state.query, state.page])
   return (
-    <AppContext.Provider value={{ ...state, removeStory }}>
+    <AppContext.Provider
+      value={{ ...state, removeStory, handleSearch, handlePage }}
+    >
       {children}
     </AppContext.Provider>
   )
