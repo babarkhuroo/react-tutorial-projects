54666fed3570804a6ded3b96a8a9733951ce0939
systemRockstar <18580997+systemRockstar@users.noreply.github.com>
2021-12-31 11:09:05 +0530

    initial commit

---
+++ 23-quiz/setup/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useGlobalContext } from './context'
+
+import SetupForm from './SetupForm'
+import Loading from './Loading'
+import Modal from './Modal'
+function App() {
+  const { waiting, loading, questions, index, correct } = useGlobalContext()
+  if (waiting) {
+    return <SetupForm />
+  }
+  if (loading) {
+    return <Loading />
+  }
+  console.log(questions)
+  const { question, incorrect_answers, correct_answer } = questions[0]
+  const answers = [...incorrect_answers, correct_answer]
+  return (
+    <main>
+      <Modal />
+      <section className='quiz'>
+        <p className='correct-answers'>
+          correct answers : {correct}/{index}
+        </p>
+      </section>
+    </main>
+  )
+}
+
+export default App
---
+++ 23-quiz/setup/src/context.js
@@ -0,0 +1,66 @@
+import axios from 'axios'
+import React, { useState, useContext, useEffect } from 'react'
+
+const table = {
+  sports: 21,
+  history: 23,
+  politics: 24,
+}
+
+const API_ENDPOINT = 'https://opentdb.com/api.php?'
+
+const url = ''
+const tempUrl =
+  'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
+
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) => {
+  const [waiting, setWaiting] = useState(true)
+  const [loading, setLoading] = useState(false)
+  const [questions, setQuestions] = useState([])
+  const [index, setIndex] = useState(0)
+  const [correct, setCorrect] = useState(0)
+  const [error, setError] = useState(false)
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const fetchQuestions = async (url) => {
+    setLoading(true)
+    setWaiting(false)
+    const response = await axios(url).catch((err) => console.log(err))
+    console.log(response)
+    if (response) {
+      const data = response.data.results
+      if (data.length > 0) {
+        setQuestions(data)
+        setLoading(false)
+        setWaiting(false)
+        setError(false)
+      } else {
+        setWaiting(true)
+        setError(true)
+      }
+    } else {
+      setWaiting(true)
+    }
+  }
+
+  useEffect(() => {
+    fetchQuestions(tempUrl)
+  }, [])
+
+  return (
+    <AppContext.Provider
+      value={(waiting, loading, questions, index, correct, error, isModalOpen)}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export { AppContext, AppProvider }
