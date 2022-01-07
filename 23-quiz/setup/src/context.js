16ecc65281b65462c1300962e51e9f5a088e24c8
systemRockstar <18580997+systemRockstar@users.noreply.github.com>
2022-01-07 21:09:54 +0530

    added modal

--- 23-quiz/setup/src/context.js
+++ 23-quiz/setup/src/context.js
@@ -50,7 +50,7 @@ const AppProvider = ({ children }) => {
     setIndex((oldIndex) => {
       const index = oldIndex + 1
       if (index > questions.length - 1) {
-        // openModal()
+        openModal()
         return 0
       } else {
         return index
@@ -63,6 +63,16 @@ const AppProvider = ({ children }) => {
       setCorrect((oldState) => oldState + 1)
     }
     nextQuestion()
+  }
+
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setWaiting(true)
+    setCorrect(0)
+    setIsModalOpen(false)
   }
 
   useEffect(() => {
@@ -81,6 +91,7 @@ const AppProvider = ({ children }) => {
         isModalOpen,
         nextQuestion,
         checkAnswer,
+        closeModal,
       }}
     >
       {children}
